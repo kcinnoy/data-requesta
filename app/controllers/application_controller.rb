19e0c942f1e0f15aca65e8cfe48f1404993e4be7
Yonnick <yonnick00@gmail.com>
2019-06-16 17:37:40 +0000

    add additional application controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    available_posts
     if logged_in?
       #redirect '/tweets'
     else
       erb :home
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      # redirect '/signup'
+      fail_signup_msg
+      redirect '/'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/posts'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(params[:id])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      fail_login_msg
+      redirect '/'
     end
   end
 
@@ -32,7 +56,22 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
     end
+
+    def fail_signup_msg
+      flash[:fail_signup]='Sign up failed. Please complete all fields and press submit'
+    end
+
+
+    def fail_login_msg
+      flash[:fail_login]='Log in failed. Please re-enter detail and try again'
+    end
+
+    def available_posts
+      if 
+    end
   end
+
+
 
 
 end
